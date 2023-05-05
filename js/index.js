91f4ba4714d82630851212aa0606b7cba6e72d3a
Abhinav Saini <ABHINAVSAINI669@GMAIL.COM>
2023-05-05 16:00:39 +0530

    added: models->project_data.js, imported in index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,42 @@
-document.getElementById("menu-icon").addEventListener("click", menuOnClick);
+import { project_data } from "../models/projects.js";
 
+console.log(project_data);
+document.getElementById("menu-icon").addEventListener("click", menuOnClick);
 function menuOnClick() {
   const navbarMain = document.getElementById("navbar-main");
   navbarMain.style.display =
     navbarMain.style.display === "none" ? "flex" : "none";
+}
+
+// const projectCard = `<div class="project">
+// <div class="project-stack">JavaScript</div>
+// <img
+//   src="https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/09/ikaw-lang-1.png?fit=650%2C390&ssl=1"
+//   alt="#"
+// />
+// <h3>Title of Project</h3>
+// <p>short description of project</p>
+// <div class="project-links">
+//   <a href="">Live</a>
+//   <a href="">Github</a>
+// </div>
+// </div>`;
+
+window.addEventListener("load", showProjects);
+function showProjects() {
+  project_data.map((project) => {
+    document.querySelector(".projects").innerHTML += `<div class="project">
+    <div class="project-stack">${project.stack}</div>
+    <img
+      src=${project.image}
+      alt=${project.name}
+    />
+    <h3>${project.title}</h3>
+    <p>${project.description}</p>
+    <div class="project-links">
+      <a href=${project.live_link} target=_blank>Live</a>
+      <a href=${project.github_link} target=_blank>Github</a>
+    </div>
+    </div>`;
+  });
 }
---
+++ models/projects.js
@@ -0,0 +1,49 @@
+const project_data = [
+  {
+    title: "QTrip Travel",
+    image: "./assets/images/projects/qtrip.png",
+    stack: "JavaScript",
+    description:
+      "QTrip is a travel website aimed at travellers looking for a multitude of adventures in different cities. ",
+    live_link: "https://qtrip-vanilla.netlify.app/",
+    github_link: "https://github.com/me4abhi/ME_QTRIPDYNAMIC",
+  },
+  {
+    title: "QKart",
+    image: "./assets/images/projects/qkart.png",
+    stack: "React.JS",
+    description:
+      "QKart is an e-commerce application offering a variety of products for customers to choose from.",
+    live_link: "https://ecom-qkart.netlify.app/",
+    github_link: "https://github.com/me4abhi/ME_QKART",
+  },
+  {
+    title: "QKart",
+    image: "./assets/images/projects/qkart.png",
+    stack: "React.JS",
+    description:
+      "QKart is an e-commerce application offering a variety of products for customers to choose from.",
+    live_link: "https://ecom-qkart.netlify.app/",
+    github_link: "https://github.com/me4abhi/ME_QKART",
+  },
+  {
+    title: "QKart",
+    image: "./assets/images/projects/qkart.png",
+    stack: "React.JS",
+    description:
+      "QKart is an e-commerce application offering a variety of products for customers to choose from.",
+    live_link: "https://ecom-qkart.netlify.app/",
+    github_link: "https://github.com/me4abhi/ME_QKART",
+  },
+  {
+    title: "QKart",
+    image: "./assets/images/projects/qkart.png",
+    stack: "React.JS",
+    description:
+      "QKart is an e-commerce application offering a variety of products for customers to choose from.",
+    live_link: "https://ecom-qkart.netlify.app/",
+    github_link: "https://github.com/me4abhi/ME_QKART",
+  },
+];
+
+export { project_data };
