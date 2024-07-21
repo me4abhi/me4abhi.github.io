6da76c862cdef5c76f1197557600db1c1742f0d6
Abhinav Saini <abhinavsaini669@gmail.com>
2024-07-21 22:24:27 +0530

    change: added 'Timeline'

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import Timeline from '../components/molecules/Timeline';
 import Header from '../components/organisms/Header';
 
 const HomePage: React.FC = () => {
   return (
     <div style={{ zIndex: '9' }}>
       <Header />
+      <Timeline />
     </div>
   );
 };
