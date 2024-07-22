e8def65be6ce4f2846bd453bee35533e7bd81471
Abhinav Saini <abhinavsaini669@gmail.com>
2024-07-22 21:46:22 +0530

    update: particles-config to star pattern

--- src/templates/ParticlesBackground/config/particlesConfig.ts
+++ src/templates/ParticlesBackground/config/particlesConfig.ts
@@ -1,119 +1,40 @@
 // import { MoveDirection, OutMode } from '@tsparticles/engine';
 
-import { IResizeEvent } from '@tsparticles/engine';
+import { IResizeEvent, MoveDirection, OutMode } from '@tsparticles/engine';
 
 const particlesConfigSquare = {
+  background: {
+    color: '#020202',
+  },
+  color: {
+    value: '#36454f',
+  },
   particles: {
     number: {
-      value: 80,
-      density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-    color: {
-      value: '#36454F',
-    },
-    shape: {
-      type: 'edge',
-      stroke: {
-        width: 0,
-        color: '#36454F',
-      },
-      polygon: {
-        nb_sides: 4,
-      },
-      image: {
-        src: 'img/github.svg',
-        width: 100,
-        height: 100,
-      },
-    },
-    opacity: {
-      value: 0.3,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
-    },
-    size: {
-      value: 10,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: false,
-      distance: 500,
-      color: '#36454F',
-      opacity: 0.4,
-      width: 2,
+      value: 100,
     },
     move: {
+      direction: MoveDirection.none,
       enable: true,
-      speed: 1,
-      direction: 0,
-      random: false,
+      outModes: {
+        default: OutMode.out,
+      },
+      random: true,
+      speed: 0.1,
       straight: false,
-      out_mode: 'out',
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200,
+    },
+    opacity: {
+      animation: {
+        enable: true,
+        speed: 1,
+        sync: false,
       },
+      value: { min: 0, max: 1 },
+    },
+    size: {
+      value: { min: 1, max: 3 },
     },
   },
-  interactivity: {
-    detect_on: 'canvas',
-    events: {
-      onhover: {
-        enable: true,
-        mode: 'bubble',
-      },
-      onclick: {
-        enable: true,
-        mode: 'repulse',
-      },
-      resize: {
-        delay: 0,
-        enable: true,
-      },
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 0.5,
-        },
-      },
-      bubble: {
-        distance: 400,
-        size: 4,
-        duration: 0.3,
-        opacity: 1,
-        speed: 3,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
-    },
-  },
-  retina_detect: true,
 };
 
 export { particlesConfigSquare };
